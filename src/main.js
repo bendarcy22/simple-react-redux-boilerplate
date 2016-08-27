d3c7dc639fb4eeddf31df67621b5bc90e6fc1e42
Ben Darcy <bendarcy22@gmail.com>
2016-08-27 12:20:25 -0400

    basic redux reducer

--- src/main.js
+++ src/main.js
@@ -3,7 +3,31 @@ import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <p>"Hello, World!"</p>,
-  document.getElementById('app')
-)
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+
+const render = () => {
+  document.body.innerText = store.getState();
+}
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () =>{
+  store.dispatch({ type: 'INCREMENT' });
+});
+
+// ReactDOM.render(
+//   <p>"Hello, World!"</p>,
+//   document.getElementById('app')
+// )
